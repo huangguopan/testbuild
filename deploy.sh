52d2054938f94b9d661a0f6d881958eac00971d6
黄国攀(Mike) <80343049@adc.com>
2022-04-06 17:32:34 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+echo Starting build process
+'/D/u3deditor/2020.3.32f1c1/Editor/Unity.exe' -quit -batchmode -projectPath ../teste -executeMethod BuildScript.PerformBuild -logFile "build.log"
+echo  Ended build process
